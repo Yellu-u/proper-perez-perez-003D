e578851536e7501ef1bf59541a25a80231fc7698
Josefina <josefinaperez230@gmail.com>
2026-06-21 01:20:30 -0400

    se crea el microservicio service-auth

---
+++ service-auth/src/main/java/com/proper/service_auth/service/AuthService.java
@@ -0,0 +1,54 @@
+package com.proper.service_auth.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.proper.service_auth.model.Rol;
+import com.proper.service_auth.model.Usuario;
+import com.proper.service_auth.repository.UsuarioRepository;
+
+@Service
+public class AuthService 
+{
+        private final UsuarioRepository usuarioRepository;
+        private final JwtService jwtService;
+        private final PasswordEncoder passwordEncoder;
+
+        public AuthService(UsuarioRepository usuarioRepository,
+                 JwtService jwtService,
+                PasswordEncoder passwordEncoder)
+                {
+                        this.usuarioRepository = usuarioRepository;
+                        this.jwtService = jwtService;
+                        this.passwordEncoder = passwordEncoder;
+                }
+
+        public String registrar(Usuario usuario)
+        {
+                usuario.setContrasena(passwordEncoder.encode(usuario.getContrasena()));
+                usuarioRepository.save(usuario);
+                return "Usuario Registrado";
+        }
+
+        public String login(String username, String password)
+        {
+                System.out.println("Intentando login para: " + username);
+
+                Usuario user = usuarioRepository.findByNombreUsuario(username)
+                        .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+                System.out.println("Usuario encontrado en la BD: " + user.getNombreUsuario());
+
+                if(passwordEncoder.matches(password, user.getContrasena()))
+                {
+                        List<String> roles = user.getRoles().stream()
+                                        .map(Rol::getNombreRol)
+                                        .collect(Collectors.toList());
+                        return jwtService.generarToken(username, roles);
+                }
+                throw new RuntimeException("Credenciales inválidas");
+        }
+}
